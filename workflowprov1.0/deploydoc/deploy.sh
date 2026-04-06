4952211dac1976b5fe3237087987ec129d1db384
Carmi Ndeke <Carmi Ndeke@MCPOP-PC-165.mshome.net>
2026-04-06 16:46:23 +0200

    My Commit in the Github

--- workflowprov1.0/deploydoc/deploy.sh
+++ workflowprov1.0/deploydoc/deploy.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-# Crée le dossier si nécessaire
-mkdir -p /home/ubuntu/workflowmicropop
+set -e  # stop si erreur
 
-# Variables Payara
+echo "=== DEPLOYMENT START ==="
+
+# Variables
 PAYARA_HOME=/opt/payara
 WAR_PATH=/home/ubuntu/workflowmicropop/workflowmicropop.war
 APP_NAME=workflowmicropop
 
-# Supprime l’ancienne version (ignore si pas installée)
+# Vérifier Payara
+echo "Checking Payara..."
+$PAYARA_HOME/bin/asadmin list-domains
+
+# Undeploy ancienne version
+echo "Undeploy old version..."
 $PAYARA_HOME/bin/asadmin undeploy $APP_NAME || true
 
-# Déploie le WAR dans le context /workflowmicropop
-$PAYARA_HOME/bin/asadmin deploy --force=true --name $APP_NAME --contextroot /workflowmicropop $WAR_PATH
+# Déployer nouvelle version
+echo "Deploying new version..."
+$PAYARA_HOME/bin/asadmin deploy \
+  --force=true \
+  --name $APP_NAME \
+  --contextroot /workflowmicropop \
+  $WAR_PATH
+
+echo "=== DEPLOYMENT SUCCESS ==="
